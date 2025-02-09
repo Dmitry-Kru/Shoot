495e29938319e27d960baeff0ce652574e4faa82
Dmitry-Kru <dukeingm@gmail.com>
2025-02-09 21:06:42 +0300

    finish

--- main.py
+++ main.py
@@ -6,6 +6,105 @@ from time import sleep
 import pygame
 from pygame.locals import *
 
+# Размеры окна
+WIDTH = 800
+HEIGHT = 600
+
+# Цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+
+
+# Загрузка изображений
+def load_image(name):
+    try:
+        image = pygame.image.load(name)
+    except pygame.error as message:
+        print("Не удалось загрузить изображение:", name)
+        raise SystemExit(message)
+    return image.convert_alpha()
+
+
+# Инициализация мишени
+def init_target(position):
+    target_image = load_image('img/target.png')
+    target_rect = target_image.get_rect(center=position)
+    speed_x = random.randint(-4, 4)
+    speed_y = random.randint(-4, 4)
+    return target_image, target_rect, speed_x, speed_y
+
+
+# Обновление положения мишени
+def update_target(target_image, target_rect, speed_x, speed_y):
+    target_rect.x += speed_x
+    target_rect.y += speed_y
+
+    if target_rect.left <= 0 or target_rect.right >= WIDTH:
+        speed_x *= -1
+    if target_rect.top <= 0 or target_rect.bottom >= HEIGHT:
+        speed_y *= -1
+
+    return target_image, target_rect, speed_x, speed_y
+
+
+# Инициализация препятствия
+def init_obstacle(position, size, color):
+    obstacle_surface = pygame.Surface(size)
+    obstacle_surface.fill(color)
+    obstacle_rect = obstacle_surface.get_rect(center=position)
+    speed_x = random.choice([-3, 3])
+    speed_y = random.choice([-3, 3])
+    return obstacle_surface, obstacle_rect, speed_x, speed_y
+
+
+# Обновление положения препятствия
+def update_obstacle(obstacle_surface, obstacle_rect, speed_x, speed_y):
+    obstacle_rect.x += speed_x
+    obstacle_rect.y += speed_y
+
+    if obstacle_rect.left <= 0 or obstacle_rect.right >= WIDTH:
+        speed_x *= -1
+    if obstacle_rect.top <= 0 or obstacle_rect.bottom >= HEIGHT:
+        speed_y *= -1
+
+    return obstacle_surface, obstacle_rect, speed_x, speed_y
+
+
+# Инициализация взрыва
+def init_explosion(center):
+    images = [
+        load_image('img/oops1.png'),
+        load_image('img/oops2.png'),
+        load_image('img/oops3.png')
+    ]
+    index = 0
+    explosion_image = images[index]
+    explosion_rect = explosion_image.get_rect(center=center)
+    timer = 0
+    return images, index, explosion_image, explosion_rect, timer
+
+
+# Обновление взрыва
+def update_explosion(images, index, explosion_image, explosion_rect, timer):
+    timer += 1
+    if timer % 10 == 0:
+        index += 1
+        if index >= len(images):
+            return None, None, None, None, None
+        else:
+            explosion_image = images[index]
+            explosion_rect = explosion_image.get_rect(center=explosion_rect.center)
+
+    return images, index, explosion_image, explosion_rect, timer
+
+
+
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Тир")
